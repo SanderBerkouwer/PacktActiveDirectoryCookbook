e53495751d6c3f22bdd6a397deca972d2c6e2f89
Sander Berkouwer <sander@berkouwer.org>
2019-04-23 15:07:20 +0200

    Create 11InstallRSATforADDS.ps1

---
+++ Chapter 1/11InstallRSATforADDS.ps1
@@ -0,0 +1,4 @@
+# Use the line below to install the Remote Server Administration Tools (RSAT) for Active Directory Domain Services:
+# Run on a domain-joined device or member server
+
+Install-WindowsFeature RSAT-AD-PowerShell
